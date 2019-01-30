f0495863cb6d1fb273cc427f5059d358f11accbe
DivyaAggarwalVinove <divya.aggarwal@mail.vinove.com>
2019-01-30 19:59:00 +0530

    Changes in appSecret for ios

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using UIKit;
 
 namespace sampleAppXamarin.iOS
@@ -13,6 +16,10 @@ namespace sampleAppXamarin.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+
+            AppCenter.Start("d9880644-275f-4841-85b2-b01404b23c3e",
+                   typeof(Analytics), typeof(Crashes));
+
             LoadApplication(new App());
 
             #if ENABLE_TEST_CLOUD
--- sampleAppXamarin/App.xaml.cs
+++ sampleAppXamarin/App.xaml.cs
@@ -11,7 +11,7 @@ namespace sampleAppXamarin
         public App()
         {
             AppCenter.Start("android=53f8bf66-ac10-4bfc-9f35-f80fd8ed67f7;" +
-                   "ios=b21e0878-63f9-4fd0-bcbf-94d1760c7f91;",
+                   "ios=d9880644-275f-4841-85b2-b01404b23c3e;",
                    typeof(Analytics), typeof(Crashes));
             MainPage = new ButtonPage();
         }
